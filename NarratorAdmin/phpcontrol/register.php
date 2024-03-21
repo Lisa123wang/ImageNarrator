37682fdd2d38fe80a14b11648e42694211b7439e
Lisa <wengling375@gmail.com>
2024-03-21 17:36:53 +0800

    basic

--- NarratorAdmin/phpcontrol/register.php
+++ NarratorAdmin/phpcontrol/register.php
@@ -1,3 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
 <?php
-
+    $dbaction=$_POST['dbaction'];
+    $account=_POST['account'];
+    $pw=$_POST['pw'];
+    $link=mysqli_connect('localhost','root','','imagenarrator');
+    if($dbaction=="insert")
+{	$sql = "insert into user (account, pw) values ('$account', '$pw')";
+    //echo $sql;
+	if(mysqli_query($link,$sql))
+	{
+		$message = "新增完成";
+			echo "<script type='text/javascript'>alert('$message'); location.href = 'index.php'</script>";
+	}
+	else
+	{
+		$message = "新增失敗";
+			echo "<script type='text/javascript'>alert('$message'); location.href = 'index.php'</script>";
+	}
+}
 ?>
+</body>
+</html>
