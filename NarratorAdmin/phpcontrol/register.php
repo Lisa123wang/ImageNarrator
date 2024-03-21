1a1f052365240d1799d664c53a3483696f17c141
Lisa <wengling375@gmail.com>
2024-03-21 17:54:16 +0800

    register success

--- NarratorAdmin/phpcontrol/register.php
+++ NarratorAdmin/phpcontrol/register.php
@@ -11,23 +11,23 @@
  <body>
 <?php
     $dbaction=$_POST['dbaction'];
-    $account=_POST['account'];
+    $account=$_POST['account'];
     $pw=$_POST['pw'];
     $link=mysqli_connect('localhost','root','','imagenarrator');
     if($dbaction=="insert")
-{	$sql = "insert into user (account, pw) values ('$account', '$pw')";
-    //echo $sql;
-	if(mysqli_query($link,$sql))
-	{
-		$message = "新增完成";
-			echo "<script type='text/javascript'>alert('$message'); location.href = 'index.php'</script>";
-	}
-	else
-	{
-		$message = "新增失敗";
-			echo "<script type='text/javascript'>alert('$message'); location.href = 'index.php'</script>";
-	}
-}
+    {	$sql = "insert into user (account, pw) values ('$account', '$pw')";
+        //echo $sql;
+        if(mysqli_query($link,$sql))
+        {
+            $message = "新增完成";
+                echo "<script type='text/javascript'>alert('$message'); location.href = '../pages-register.html'</script>";
+        }
+        else
+        {
+            $message = "新增失敗";
+                echo "<script type='text/javascript'>alert('$message'); location.href = '../pages-register.html'</script>";
+        }
+    }
 ?>
 </body>
 </html>
