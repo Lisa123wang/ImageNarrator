8fdcab8a5a31bc9691cd9a9b54b907678dd7c42a
chunyinyang <fallnight5137@gmail.com>
2024-04-06 23:00:47 +0800

    update

--- NarratorAdmin/phpcontrol/logincheck.php
+++ NarratorAdmin/phpcontrol/logincheck.php
@@ -2,11 +2,11 @@
     session_start();
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $nickname = $_POST['nickname'];
 
-
+    
     $link = mysqli_connect('localhost', 'root', '', 'narratordb_test1');
-    $sql = "SELECT distinct * FROM user WHERE email = '$email' and password = '$password'";
+    // $sql = "SELECT distinct * FROM user WHERE email = '$email' and password = '$password'";
+    $query = "SELECT userID, email, nickname name FROM user WHERE email = ? AND password = ?"; 
 
     $result = mysqli_query($link, $sql);
     if($row=mysqli_fetch_assoc($result))
