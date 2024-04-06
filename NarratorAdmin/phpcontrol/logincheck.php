8a31074797a173c75db6d6a58d8903b73d768931
chunyinyang <fallnight5137@gmail.com>
2024-04-07 00:05:10 +0800

    update

--- NarratorAdmin/phpcontrol/logincheck.php
+++ NarratorAdmin/phpcontrol/logincheck.php
@@ -6,11 +6,13 @@
     
     $link = mysqli_connect('localhost', 'root', '', 'narratordb_test1');
     $sql = "SELECT distinct * FROM user WHERE email = '$email' and password = '$password'";
+    $sql = "SELECT user.email, profile.nickname FROM user INNER JOIN profile ON user.id = profile.user_id WHERE user.email = '$email' AND user.password = '$password' LIMIT 1";
     
     $result = mysqli_query($link, $sql);
     if($row=mysqli_fetch_assoc($result))
     {
         $_SESSION['email'] = $row['email'];
+        $_SESSION['nickname'] = $row['nickname'];
         header("Location:../pages-dashboard.php?method=message&message=登入成功");
         exit;
 
