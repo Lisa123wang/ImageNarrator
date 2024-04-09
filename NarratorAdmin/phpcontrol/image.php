3e36506556094e1e2ba1b891599c50d68ebd5630
chunyinyang <fallnight5137@gmail.com>
2024-04-09 22:55:51 +0800

    update image.php

--- NarratorAdmin/phpcontrol/image.php
+++ NarratorAdmin/phpcontrol/image.php
@@ -1,23 +1,26 @@
 <?php
 
-if (isset($_GET['email'])) {
-    $email = $_GET['email'];
+    session_start();
 
-    // 透過資料庫獲得使用者個人圖片
-    $sql = "SELECT profileImg FROM profile WHERE email = ?";
-    $stmt = mysqli_prepare($link, $sql);
-    mysqli_stmt_bind_param($stmt, "s", $email);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result($stmt, $image);
-    mysqli_stmt_fetch($stmt);
+    if (isset($_GET['email'])) {
+        $email = $_GET['email'];
 
-    if ($image) {
-        header("Content-Type: image/jpeg"); // 假設圖片是JPEG格式
-        echo $image;
-    } else {
-        readfile('default/path/to/image.jpg');
+        // 透過資料庫獲得使用者個人圖片
+        $link = mysqli_connect('localhost', 'root', '', 'narratordb_test1');
+        $sql = "SELECT profileImg FROM profile WHERE email = ?";
+        $stmt = mysqli_prepare($link, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $image);
+        mysqli_stmt_fetch($stmt);
+
+        if ($image) {
+            header("Content-Type: image/jpeg"); // 假設圖片是JPEG格式
+            echo $image;
+        } else {
+            readfile('default/path/to/image.jpg');
+        }
+        exit;
     }
-    exit;
-}
 
 ?>
