6d1140d01b1bf8c4ced099043786afc7e6f46e34
chunyinyang <fallnight5137@gmail.com>
2024-04-10 11:28:54 +0800

    update register.php

--- NarratorAdmin/phpcontrol/register.php
+++ NarratorAdmin/phpcontrol/register.php
@@ -1,33 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
- <head>
-  <meta charset="UTF-8">
-  <meta name="Generator" content="EditPlus®">
-  <meta name="Author" content="">
-  <meta name="Keywords" content="">
-  <meta name="Description" content="">
-  <title>Document</title>
- </head>
- <body>
 <?php
     $dbaction=$_POST['dbaction'];
     $email=$_POST['email'];
     $password=$_POST['password'];
     $link=mysqli_connect('localhost','root','','narratordb_test1');
-    if($dbaction=="insert")
-    {	$sql = "insert into user (email, password) values ('$email', '$password')";
-        //echo $sql;
-        if(mysqli_query($link,$sql))
-        {
+    
+    if ($dbaction == "insert") {
+
+        mysqli_begin_transaction($link);
+    
+        try {
+            // 透過使用者輸入的 email,password 在user表中新增一筆新的資料
+            $sqlUser = "INSERT INTO user (email, password) VALUES (?, ?)";
+            $stmtUser = mysqli_prepare($link, $sqlUser);
+            mysqli_stmt_bind_param($stmtUser, "ss", $email, $password);
+            mysqli_stmt_execute($stmtUser);
+            $userId = mysqli_insert_id($link); // 獲得剛建立的userID
+    
+            // 在profile表中為此為使用者新增空值
+            $sqlProfile = "INSERT INTO profile (userID) VALUES (?)";
+            $stmtProfile = mysqli_prepare($link, $sqlProfile);
+            mysqli_stmt_bind_param($stmtProfile, "i", $userId);
+            mysqli_stmt_execute($stmtProfile);
+    
+            mysqli_commit($link);
+    
             $message = "新增完成";
-                echo "<script type='text/javascript'>alert('$message'); location.href = '../index.php'</script>";
-        }
-        else
-        {
-            $message = "新增失敗";
-                echo "<script type='text/javascript'>alert('$message'); location.href = '../pages-register.php'</script>";
+            echo "<script type='text/javascript'>alert('$message'); location.href = '../index.php';</script>";
+        } catch (Exception $e) {
+            // 新增失敗
+            mysqli_rollback($link);
+            $message = "新增失敗：" . $e->getMessage();
+            echo "<script type='text/javascript'>alert('$message'); location.href = '../pages-register.php';</script>";
         }
     }
+    mysqli_close($link);
+
 ?>
-</body>
-</html>
