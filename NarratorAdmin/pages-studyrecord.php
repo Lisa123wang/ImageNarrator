51b092b8e2fe45bfcf47dc5ae33f5c12061e1057
Lisa <wengling375@gmail.com>
2024-05-06 11:43:27 +0800

    Update pages-studyrecord.php

--- NarratorAdmin/pages-studyrecord.php
+++ NarratorAdmin/pages-studyrecord.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <!-- DataTables CSS -->
@@ -13,11 +13,9 @@
     <!-- Favicons -->
     <link href="assets/img/favicon.png" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -26,10 +24,12 @@
     <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
     <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
-
+    <!-- ApexCharts -->
+    <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
     <!-- =======================================================
     * Template Name: NiceAdmin
     * Updated: Jan 29 2024 with Bootstrap v5.3.2
@@ -37,9 +37,8 @@
     * Author: BootstrapMade.com
     * License: https://bootstrapmade.com/license/
     ======================================================== -->
-
 </head>
-
+<body>
 <?php
     session_start();
     $link = mysqli_connect('localhost', 'root', '', 'narratordb_test1');
@@ -55,229 +54,199 @@
         echo " (" . htmlspecialchars($_SESSION['nickname']) . ")";
     }
 
-    $email = $_SESSION['email'];
-    $sqlUser = "SELECT userID FROM user WHERE email = ?";
-    $stmtUser = mysqli_prepare($link, $sqlUser);
-    mysqli_stmt_bind_param($stmtUser, "s", $email);
-    mysqli_stmt_execute($stmtUser);
-    $resultUser = mysqli_stmt_get_result($stmtUser);
-    $user = mysqli_fetch_assoc($resultUser);
-    $userID = $user['userID'];
-
-    // Revised query to get screenshot counts by date
-    $sqlScreenshotCount = "SELECT dateCreated AS date, COUNT(*) AS scshotCount FROM imagerecognition WHERE userID = ? GROUP BY dateCreated ORDER BY dateCreated DESC";
+    // Revised query to get screenshot counts by month for all users
+    $sqlScreenshotCount = "SELECT DATE_FORMAT(dateCreated, '%Y-%m') AS month, COUNT(*) AS scshotCount
+                           FROM imagerecognition
+                           GROUP BY month
+                           ORDER BY month";
     $stmtScreenshotCount = mysqli_prepare($link, $sqlScreenshotCount);
-    mysqli_stmt_bind_param($stmtScreenshotCount, "i", $userID);
     mysqli_stmt_execute($stmtScreenshotCount);
     $resultScreenshotCount = mysqli_stmt_get_result($stmtScreenshotCount);
+
+    // Set up an array for all months
+    $all_months = [
+        "January", "February", "March", "April", "May", "June", 
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    $labels = [];
+    $counts = [];
+    $current_year = date("Y");
+    for ($month = 1; $month <= 12; $month++) {
+        $labels[] = $all_months[$month - 1] . " " . $current_year;
+        $counts[] = 0;
+    }
+
+    // Update counts based on the result
+    while ($row = mysqli_fetch_assoc($resultScreenshotCount)) {
+        $month_year = explode("-", $row['month']);
+        $month = intval($month_year[1]);
+        $month_name = $all_months[$month - 1] . " " . $month_year[0];
+        $index = array_search($month_name, $labels);
+        if ($index !== false) {
+            $counts[$index] = intval($row['scshotCount']);
+        }
+    }
 ?>
 
-<body>
-     <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top d-flex align-items-center">
+<header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="d-flex align-items-center justify-content-between">
+        <a href="index.html" class="logo d-flex align-items-center">
+            <img src="assets/img/imageNarrator logo.png" alt="">
+            <span class="d-none d-lg-block">IMAGE NARRATOR</span>
+        </a>
+        <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
 
-<div class="d-flex align-items-center justify-content-between">
-  <a href="index.html" class="logo d-flex align-items-center">
-    <img src="assets/img/imageNarrator logo.png" alt="">
-    <span class="d-none d-lg-block">IMAGE NARRATOR</span>
-  </a>
-  <i class="bi bi-list toggle-sidebar-btn"></i>
-</div><!-- End Logo -->
-
-<nav class="header-nav ms-auto">
-    <ul class="d-flex align-items-center">
-
-        <li>
-            <a class="nav-link nav-icon" href="https://chrome.google.com/webstore/detail/summary-for-google-with-c/cmnlolelipjlhfkhpohphpedmkfbobjc">
-                <i class="bx bxl-google"></i>
-
-            </a><!-- End chrome Icon -->
-        </li>
-
-        <li class="nav-item dropdown">
-
-            <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                <i class="bi bi-bell"></i>
-                <span class="badge bg-primary badge-number">1</span>
-            </a><!-- End Notification Icon -->
-
-            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                <li class="dropdown-header">
-                    You have 1 new notifications
-                    <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                </li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-
-                <li class="notification-item">
-                    <i class="bi bi-info-circle text-primary"></i>
-                    <div>
-                        <h4>Dicta reprehenderit</h4>
-                        <p>Quae dolorem earum veritatis oditseno</p>
-                        <p>4 hrs. ago</p>
-                    </div>
-                </li>
-
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <!--
-        <li class="dropdown-footer">
-          <a href="#">Show all notifications</a>
-        </li>
-        -->
-            </ul><!-- End Notification Dropdown Items -->
-
-        </li><!-- End Notification Nav -->
-
-        <li class="nav-item dropdown">
-
-            <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                <i class="bi bi-clock"></i>
-                <span class="badge bg-success badge-number">3.5</span>
-            </a><!-- End Messages Icon -->
-
-        </li><!-- End Messages Nav -->
-
-        <li class="nav-item dropdown pe-3">
-
-            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo htmlspecialchars($_SESSION['nickname']); ?></span>
-            </a><!-- End Profile Iamge Icon -->
-
-            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-                <li class="dropdown-header">
-                    <h6><?php echo htmlspecialchars($_SESSION['nickname']); ?></h6>
-                    <span>user</span>
-                </li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-
-                <li>
-                    <a class="dropdown-item d-flex align-items-center" href="pages-profile.php">
-                        <i class="bi bi-person"></i>
-                        <span>My Profile</span>
-                    </a>
-                </li>
-                
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-
-                <li>
-                    <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                        <i class="bi bi-question-circle"></i>
-                        <span>Need Help?</span>
-                    </a>
-                </li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-
-                <li>
-                    <a class="dropdown-item d-flex align-items-center" href="phpcontrol/logout.php">
-                        <i class="bi bi-box-arrow-right"></i>
-                        <span>Sign Out</span>
-                    </a>
-                </li>
-
-            </ul><!-- End Profile Dropdown Items -->
-        </li><!-- End Profile Nav -->
-
-    </ul>
-</nav><!-- End Icons Navigation -->
-
-</header><!-- End Header -->
-    <!-- ======= Sidebar ======= -->
-    <aside id="sidebar" class="sidebar">
-
-        <ul class="sidebar-nav" id="sidebar-nav">
-
-            <li class="nav-item">
-                <a class="nav-link " href="#">
-                    <i class="bi bi-grid"></i>
-                    <span>Dashboard</span>
-                </a>
-            </li><!-- End Dashboard Nav -->
-
-            <li class="nav-heading"><font color="#333">Pages</li>
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="pages-profile.php">
-                    <i class="bi bi-person"></i>
-                    <span>Profile</span>
-                </a>
-            </li><!-- End Profile Page Nav -->
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="pages-video.php">
-                    <i class="bi bi-person"></i>
-                    <span>Videos</span>
-                </a>
-            </li><!-- End Video Page Nav -->
-
+    <nav class="header-nav ms-auto">
+        <ul class="d-flex align-items-center">
+            <li>
+                <a class="nav-link nav-icon" href="https://chrome.google.com/webstore/detail/summary-for-google-with-c/cmnlolelipjlhfkhpohphpedmkfbobjc">
+                    <i class="bx bxl-google"></i>
+                </a><!-- End chrome Icon -->
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                    <i class="bi bi-bell"></i>
+                    <span class="badge bg-primary badge-number">1</span>
+                </a><!-- End Notification Icon -->
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+                    <li class="dropdown-header">
+                        You have 1 new notifications
+                        <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                    </li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li class="notification-item">
+                        <i class="bi bi-info-circle text-primary"></i>
+                        <div>
+                            <h4>Dicta reprehenderit</h4>
+                            <p>Quae dolorem earum veritatis oditseno</p>
+                            <p>4 hrs. ago</p>
+                        </div>
+                    </li>
+                    <li><hr class="dropdown-divider"></li>
+                </ul><!-- End Notification Dropdown Items -->
+            </li><!-- End Notification Nav -->
+            <li class="nav-item dropdown">
+                <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                    <i class="bi bi-clock"></i>
+                    <span class="badge bg-success badge-number">3.5</span>
+                </a><!-- End Messages Icon -->
+            </li><!-- End Messages Nav -->
+            <li class="nav-item dropdown pe-3">
+                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                    <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo htmlspecialchars($_SESSION['nickname']); ?></span>
+                </a><!-- End Profile Iamge Icon -->
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                    <li class="dropdown-header">
+                        <h6><?php echo htmlspecialchars($_SESSION['nickname']); ?></h6>
+                        <span>user</span>
+                    </li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="pages-profile.php">
+                            <i class="bi bi-person"></i>
+                            <span>My Profile</span>
+                        </a>
+                    </li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                            <i class="bi bi-question-circle"></i>
+                            <span>Need Help?</span>
+                        </a>
+                    </li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="phpcontrol/logout.php">
+                            <i class="bi bi-box-arrow-right"></i>
+                            <span>Sign Out</span>
+                        </a>
+                    </li>
+                </ul><!-- End Profile Dropdown Items -->
+            </li><!-- End Profile Nav -->
         </ul>
+    </nav><!-- End Icons Navigation -->
+</header><!-- End Header -->
 
-    </aside><!-- End Sidebar-->
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link " href="#">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+        <li class="nav-heading"><font color="#333">Pages</font></li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-profile.php">
+                <i class="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-video.php">
+                <i class="bi bi-person"></i>
+                <span>Videos</span>
+            </a>
+        </li><!-- End Video Page Nav -->
+    </ul>
+</aside><!-- End Sidebar-->
 
-    <main id="main" class="main">
+<main id="main" class="main">
     <div class="pagetitle">
-            <h1>Dashboard</h1>
+        <h1>Dashboard</h1>
+    </div>
 
-            <nav>
-
-                
-            </nav>
-            
-           
-        </div>
-        <!-- Add this HTML block where you want the table to appear -->
-        <!-- Add this HTML block where you want the table to appear -->
-        <div style="width: 100%; background-color: white; border-radius: 10px; padding: 10px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
-            <h5><b></b></h5>
-            <table id="recVideoTable" class="display" style="width: 100%;">
-                
-                <tbody>
-                    
-                </tbody>
-            </table>
-        </div>
-
-
+    <!-- Number of Screenshots by Month Table -->
+    <div style="width: 100%; background-color: white; border-radius: 10px; padding: 10px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
         
-    </main>
-
-    <script>
-        $(document).ready(function () {
-            // Initialize DataTable
-            $('#screenshotTable').DataTable();
-        });
-    </script>
+    <div id="barChart"></div>
 
 <script>
-        $(document).ready(function () {
-            // Initialize DataTable for the Right Table
-            $('#subjectsTable').DataTable();
-        });
-        $(document).ready(function () {
-            // Initialize DataTable for the Right Table
-            $('#subjectsTable2').DataTable();
-        });
-    </script>
-    <!-- ======= Footer ======= -->
+    $(document).ready(function () {
+        $('#monthScreenshotTable').DataTable();
+
+        var options = {
+            series: [{
+                name: 'Screenshots',
+                data: <?php echo json_encode($counts); ?>
+            }],
+            chart: {
+                type: 'bar',
+                height: 400
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '50%'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            xaxis: {
+                categories: <?php echo json_encode($labels); ?>
+            }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#barChart"), options);
+        chart.render();
+    });
+</script>
+
+    </div>
+
     <footer id="footer" class="footer">
         <div class="copyright">
             &copy; Copyright <strong><span>IMAGE NARRATOR</span></strong>. All Rights Reserved
         </div>
     </footer><!-- End Footer -->
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i>Back to Top</a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>Back to Top
+    </a>
 
     <!-- Vendor JS Files -->
     <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
@@ -291,6 +260,5 @@
 
     <!-- Template Main JS File -->
     <script src="assets/js/main.js"></script>
-
 </body>
 </html>
