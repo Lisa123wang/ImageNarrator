0f7a9a6a1a67e8c2dd797a65c24048a5902133f1
Lisa <wengling375@gmail.com>
2024-05-07 12:07:39 +0800

    delete

--- NarratorAdmin/pages-user.php
+++ NarratorAdmin/pages-user.php
@@ -183,23 +183,13 @@
                     <hr class="dropdown-divider">
                 </li>
 
-                <li>
-                    <a class="dropdown-item d-flex align-items-center" href="pages-profile.php">
-                        <i class="bi bi-person"></i>
-                        <span>My Profile</span>
-                    </a>
-                </li>
+                
                 
                 <li>
                     <hr class="dropdown-divider">
                 </li>
 
-                <li>
-                    <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                        <i class "bi bi-question-circle"></i>
-                        <span>Need Help?</span>
-                    </a>
-                </li>
+                
                 <li>
                     <hr class="dropdown-divider">
                 </li>
