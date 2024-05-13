33c0fa809867492fbcac67345f2c1f5fa7b753b9
Lisa <wengling375@gmail.com>
2024-05-13 21:22:44 +0800

    Update pages-members.php

--- NarratorAdmin/pages-members.php
+++ NarratorAdmin/pages-members.php
@@ -44,6 +44,9 @@
 <body>
 <?php
     session_start();
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
     $link = mysqli_connect('localhost', 'root', '', 'narratordb_test1');
 
     if(!isset($_SESSION['email'])) {
